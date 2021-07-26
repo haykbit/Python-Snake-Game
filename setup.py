244d2179289b4e84a778befe15c932898acc35af
Hayk Petrosyan <hpetrosyan.projects@gmail.com>
2021-07-26 22:10:02 +0200

    fix: fixed empty var error

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     description='A snake game programed with python',
     license="HaykPetrosyanElmayan",
-    long_description=long_description,
+    long_description="Building snake and apple game in pygame and python Snake and apple is a simple game where when snake eats an apple it gets longer and your score increases. If you snake hits any part of its body it will die.",
     author='Hayk Petrosyan',
     author_email='hpetrosyan.projects@gmail.com',
     packages=['snake-game'],  # same as name
