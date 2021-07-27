e9f66ee2ab8594858cb7cb141e371fc294976243
Hayk Petrosyan <hpetrosyan.projects@gmail.com>
2021-07-27 03:01:06 +0200

    feat: Class Apple created

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import pygame
+from pygame.locals import *
+import time
+import random
 
-if __name__ == "__main__":
-    pygame.init()
+SIZE = 30
+SIZE_IMAGE = 10
 
-    surface = pygame.display.set_mode((500,500))
+
+class Apple:
+    def __init__(self, surface):
+        self.parent_screen = surface
+        self.image = pygame.image.load("resources/apple-block.jpg").convert()
+        self.x = SIZE_IMAGE*3
+        self.y = SIZE_IMAGE*3
+
+    def draw(self):
+        self.parent_screen.blit(self.image, (self.x, self.y))
+        pygame.display.flip()
+
+    def move(self):
+        self.x = random.randint(0, 30)*SIZE
+        self.y = random.randint(0, 23)*SIZE
