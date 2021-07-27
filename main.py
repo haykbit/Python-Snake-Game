292f7ca3d6b79b43db2a95916da4af1d59606396
Hayk Petrosyan <hpetrosyan.projects@gmail.com>
2021-07-27 03:01:37 +0200

    feat: class game added

--- main.py
+++ main.py
@@ -21,3 +21,59 @@ class Apple:
     def move(self):
         self.x = random.randint(0, 30)*SIZE
         self.y = random.randint(0, 23)*SIZE
+
+
+class Snake:
+    def __init__(self, surface, length):
+        # block length
+        self.length = length
+        # Parent screen for acces surface
+        self.parent_screen = surface
+        # Load image
+        self.block = pygame.image.load("resources/snake-block.jpg").convert()
+        # set multiple x and y cordinate
+        self.x = [SIZE]*length
+        self.y = [SIZE]*length
+        # Set first direction
+        self.direction = 'down'
+
+    # method draw snake block
+    def draw(self):
+        self.parent_screen.fill((31, 31, 31))
+        for i in range(self.length):
+            self.parent_screen.blit(self.block, (self.x[i], self.y[i]))
+        pygame.display.flip()
+
+    def increase_length(self):
+        self.length += 1
+        self.x.append(-1)
+        self.y.append(-1)
+
+    def move_left(self):
+        self.direction = 'left'
+
+    def move_right(self):
+        self.direction = 'right'
+
+    def move_up(self):
+        self.direction = 'up'
+
+    def move_down(self):
+        self.direction = 'down'
+
+    def walk(self):
+
+        for i in range(self.length-1, 0, -1):
+            self.x[i] = self.x[i - 1]
+            self.y[i] = self.y[i - 1]
+
+        if self.direction == 'up':
+            self.y[0] -= SIZE
+        if self.direction == 'down':
+            self.y[0] += SIZE
+        if self.direction == 'left':
+            self.x[0] -= SIZE
+        if self.direction == 'right':
+            self.x[0] += SIZE
+
+        self.draw()
